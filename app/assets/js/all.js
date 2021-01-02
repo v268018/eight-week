a8315b132c364702ab14d0e61bed25f31d1e1e7f
v268018 <v268018_0322@yahoo.co.jp>
2021-01-03 02:17:50 +0800

    first-commane

---
+++ app/assets/js/all.js
@@ -0,0 +1,19 @@
+//swiper
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,//一次顯示幾張圖片
+  //spaceBetween: 30,//每張圖之間距離
+  loop:true ,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay:true,//自動輪播
+});
+
+
+
+
